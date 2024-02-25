d959d5a49ab6355c68f9ce481a1a1256e708af34
Daniel Querales <d.querales@gmail.com>
2024-02-25 12:46:30 -0400

    update train model

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 TARGET = "price"
+FEATURES = ['volume']
 CURRENT = "data/current/current.csv"
 SOURCE = "https://data.bitcoinity.org/export_data.csv?currency=USD&data_type=price_volume&t=lb&timespan=30d&vu=curr"
--- src/train_model.py
+++ src/train_model.py
@@ -1,26 +1,31 @@
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
 import config
 
 df = pd.read_csv(config.SOURCE)
 df.to_csv(config.CURRENT, index=False)
 
-X = df.drop([config.TARGET], axis=1)
+X = df[[config.FEATURES]]
 y = df[config.TARGET]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
-model = LogisticRegression()
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-print("Logistic Regression \n")
-print("accuracy: ", accuracy_score(y_test, y_pred))
+# The coefficients
+print("Coefficients: \n", model.coef_)
+# The mean squared error
+print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+# The coefficient of determination: 1 is perfect prediction
+print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
 
 pickle.dump(model, open("model/model.pkl", "wb"))
